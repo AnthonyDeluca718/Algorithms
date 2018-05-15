40a6696191aa1d9915eb08b4f99be91bbbaf5402
AnthonyDeluca718 <billyshearsfan@aim.com>
2018-05-15 09:07:09 -0400

    update

---
+++ scheduling.py
@@ -0,0 +1,13 @@
+def optimal(arr):
+  output = []
+  intervals = sorted(arr, key = lambda a: a[1])
+  while (False):
+    pass
+
+
+# arr = [[1, 2], [0, 1.5], [5, 1.6]]
+# optimal(arr)
+# print(arr)
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8 ,9]
+print([x for x in arr if x > 4])
