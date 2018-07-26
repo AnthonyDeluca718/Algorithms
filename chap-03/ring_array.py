402a98225a93c4273826a20dcaaaf40652c8948a
Anthony <anthony@expandtheroom.com>
2018-07-26 16:43:25 -0400

    pop added

--- chap-03/ring_array.py
+++ chap-03/ring_array.py
@@ -1,7 +1,7 @@
 # shift
 # unshift
 # push - done
-# pop
+# pop - done
 # set - done
 # get - done
 # length - done
@@ -27,10 +27,12 @@ class RingArray:
     return (self.start + idx) % self.max_len
 
   def get(self, idx):
-    return self.arr.get(self.index(idx))
+    if (idx < self.length):
+      return self.arr.get(self.index(idx))
 
   def set(self, idx, val):
-    self.arr.set(self.index(idx), val)
+    if (idx < self.length):
+      self.arr.set(self.index(idx), val)
 
   def rebuild(self):
     self.max_len = 2*self.max_len
@@ -42,26 +44,39 @@ class RingArray:
 
   def push(self, val):
     if (self.length < self.max_len):
-      self.set(self.length, val)
+      self.arr.set(self.index(self.length), val)
       self.length = self.length + 1
     else:
       self.rebuild()
       self.push(val)
 
+  def pop(self):
+    if (self.length > 0):
+      ret = self.get(self.length - 1)
+      self.length = self.length - 1
+      return ret
+
 
 my_array = RingArray()
 my_array.push(1)
 my_array.push(2)
-my_array.set(1, 10)
+my_array.set(0, 10)
 my_array.push(3)
 my_array.push(4)
 my_array.push(5)
 my_array.push(6)
 my_array.set(3, -10)
 
-print(my_array.get(0))
-print(my_array.get(1))
-print(my_array.get(2))
-print(my_array.get(3))
-print(my_array.get(4))
-print(my_array.get(5))
+print(my_array.pop())
+print(my_array.pop())
+print(my_array.pop())
+print(my_array.pop())
+print(my_array.pop())
+print(my_array.pop())
+
+# print(my_array.get(0))
+# print(my_array.get(1))
+# print(my_array.get(2))
+# print(my_array.get(3))
+# print(my_array.get(4))
+# print(my_array.get(5))
